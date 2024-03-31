cfe9dd6c0b8349f9251b3df0fe7fb57d20b9b650
Jacob Wright <jacob.wright.ut@gmail.com>
2024-03-31 01:16:50 -0600

    add: demographics screen basic

--- components/Onboarding/UserInfo/Demographics.tsx
+++ components/Onboarding/UserInfo/Demographics.tsx
@@ -1,16 +1,46 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
 import React from 'react'
+import NextButton from '@/components/NextButton'
 
-type Props = {}
+type Props = {
+  onPress: () => void
+}
 
 const Demographics = (props: Props) => {
   return (
-    <SafeAreaView>
-      <Text>Demographics</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Let’s get to know you a little better!</Text>
+      <Text style={styles.subtitle}>
+        Answer the following prompts below to access your potential
+      </Text>
+    </View>
   )
 }
 
 export default Demographics
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    fontFamily: 'DMSerifDisplay',
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: 'left',
+    marginVertical: 20,
+    fontFamily: 'Inter',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+})
